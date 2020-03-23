1db7590655bb3c9e2d21a2020c4d21f6626035a9
DESKTOP-JP-HOME\Tebbe <jptebbe@gmail.com>
2020-03-23 23:13:37 +0100

    refactoring & format changes - one more task for v0.1 open

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
+#############################################
+############       Boulder Tracker
+############            V 0.1
+############        @Thukyd, 2020
+#############################################
+
+
+
 import datetime
 import time
 import logging
 
-######### MongoDB
+# Needed for MongoDB
 import json
 import pymongo
 from pymongo import MongoClient
 
-######### Local DB
-# import json # duplicate Mongo
+# Needed for local JSON
+# import json
 import os
 import logging
 
-
-# TODO: https://www.youtube.com/watch?v=rE_bJl2GAY8
-# TODO: https://account.mongodb.com/account/login
-###     Connection to MongoDB
-### Design: https://www.mongodb.com/blog/post/building-with-patterns-a-summary?utm_campaign=Int_EM_ONB_FT1a_10_19_WW&utm_source=Eloqua&utm_medium=email&utm_term=Designing%20a%20MongoDB%20schema%20for%20Atlas
+######### 
+# Storage Option A: MongoDB Cloud
 class OptionMongoDb():
     def __init__(self):
         self.collection = self.config()
 
+    # sets up MongoDB connection
     def config(self):
         with open("./mongo/configurations.json") as file:
             parsed = json.load(file)
@@ -34,11 +40,13 @@ class OptionMongoDb():
             collection = db[param_collection]
         return collection
 
+    # inserts records & user into DB
     def insert(self, data):
         self.collection.insert_one(
             data
         )
 
+    # updates "user" documents with new "record" array 
     def add_record(self, user_id, record):
         self.collection.update_one({
             "_id" : user_id
@@ -47,6 +55,7 @@ class OptionMongoDb():
             "$push" : {"records": record}
         })
 
+    # checks if there is already an entry for this user
     def check_user_existance(self, user_id):
         # returns "find" returns mongo object that needs => needs to be handled in loops
         if self.collection.find({"_id" : user_id}).count() > 0: 
@@ -54,15 +63,14 @@ class OptionMongoDb():
         else:
             return False
 
-    def find_entry(self, user_id):
-        return self.collection.find({"_id": user_id})
-
-###     Connection to a local storage
+######### 
+# Storage Option B: JSON in Local Folder
 class OptionLocalStorage():
     def __init__(self):
         self.path = "./local/user_records.json"
         self.startupCheck()
 
+    # check if file exists
     def startupCheck(self):
         # check if file exists
         if os.access(self.path, os.R_OK):
@@ -73,15 +81,15 @@ class OptionLocalStorage():
                 json.dump(structure, file)
             logging.warning("   startupCheck: user_records.json was created")
 
+    # searches for user id
     def search_user(self, user_id, data):
         # returns matching dictionary
         return [element for element in data if element['_id'] == user_id]
     
+    # adds a new user entry
     def insert(self, data):
-
         user = data[0]
         record = data[1]
-
         try: 
             with open(self.path, "r+") as file:
                 json_file = json.load(file)
@@ -106,6 +114,8 @@ class OptionLocalStorage():
         except RuntimeError:
             print("     Error: \n Could not add entry.")
 
+######### 
+# Basic Operations
 class Operations():
     def __init__(self):
         self.mongo_storage = self.storage_config()
@@ -113,72 +123,65 @@ class Operations():
         self.mappings = self.get_mapping()
         self.option = self.get_app_options()
 
-    # check if mongoDB or local storage should be user
+    # checks if mongoDB or local storage should be used
     def storage_config(self):
         with open("./mongo/configurations.json") as file:
             parsed = json.load(file)
             mongo_db_status = parsed["activated"]
         return mongo_db_status
     
+    # checks if mongoDB is activated in options; otherwise the local storage is active
     def get_use_mongo_status(self):
         return self.mongo_storage
 
-    # check for current user
+    # returns current user from config json
     def get_user(self):
         with open("./config/user_identity.json") as file:
             current_user = json.load(file)
         return current_user
 
+    # sets a new user in config json
     def set_user(self, data):
         with open("./config/user_identity.json", "w") as file:
             json.dump(data, file)
 
+    # returns scale type and competion type mapping
     def get_mapping(self):
         with open("./config/mappings.json") as file:
             mapping = json.load(file)
         return mapping
 
+    # return which scale type is used currently
     def get_app_options(self):
         with open("./config/options.json") as file:
             options = json.load(file)
         return options
 
-    def set_app_options(self):
-        # TODO
-        pass
-        
-
-    ################ Adding Entries
-        
+    # adds an entry in mongoDB    
     def add_entry_mongo(self, record_input):
         record = record_input
         user = self.current_user
 
+        #TODO: THIS LOGIC BELLOW IS PART OF MONGODB OPERATIONS => 
+        # cut & paste part bellow into OptionMongoDB, similar to add_entry_local
         # existing user? => add record
         if OptionMongoDb().check_user_existance(user["_id"]): 
             entry = record
             OptionMongoDb().add_record(user["_id"], record)
-            # TEST BEGIN
-            user_id = user["_id"]
-            print(f"{user_id} existiert bereits. Neuer Record hinzugefügt")
-            # TEST END
         # new user? => create user + record
         else:
             entry = user
             entry["records"] = [record]
             OptionMongoDb().insert(entry)
-            # TEST BEGIN
-            user_id = user["_id"]
-            print(f"Neue Nutzer mit der ID {user_id} angelegt")
-            # TEST END
 
+    # adds an entry in local folder  
     def add_entry_local(self, record_input):
         record = record_input
         user = self.current_user
         OptionLocalStorage().insert([user, record])
 
-
-###     Boulder Grades
+######### 
+# Boulder Grades/Scale
 class Grades():
     def __init__(self):
         # mapping based on this =>
@@ -187,31 +190,31 @@ class Grades():
         self.scale_type = self.scale_mapping["name"]
         self.grades = self.scale_mapping["grade"]
 
+    # returns scale type mapping
     def get_mapping(self):
         return Operations().get_mapping()["scale_type"]
 
+    # sets new scale type standard in options.json
     def set_scale_standard(self):
         # TODO => calls up local/options and changes this variable
         # expects an integer from 0 till 2
         pass
 
+    # converts input into a scale
     def convert_grade(self, input_grade, output_scale):
         #TODO Scale type is from __init__
         pass
 
-    #TODO OBSOLETE IN FUTURE?
-    def set_route_grade(self, index):
-        scale = Operations().get_app_options()["standard_scale"]
-        current_grade = self.scale_mapping["grade"][index][scale]
-        return current_grade
-
-###     Boulder Completion Types
+######### 
+# Completion Types
 class CompletionType():
     def __init__(self):
         self.type = self.get_mapping()
 
+    # returns index mapping of completion types in mappings.json
     def get_mapping(self):
         return Operations().get_mapping()["completion_type"]
 
+    # sets completion type based on mappings.json
     def set_completion_type(self, index):
         return self.type[index]
